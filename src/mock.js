0b6d3cbf0525b63b5f40b999d13cf5ae254a00b7
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-09-21 19:44:49 +0500

    test

---
+++ src/mock.js
@@ -0,0 +1,6 @@
+export const users = [
+    { id: 1, name: "Ashrafxon", status: "developer" },
+    { id: 2, name: "Nodirbek", status: "freelancer" },
+    { id: 3, name: "Shexroz", status: "engineer" },
+    { id: 4, name: "Damirbek", status: "mentor" },
+  ];
